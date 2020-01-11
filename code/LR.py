b42a4be812854c77e13bf0f508c3f60ab50e8fca
国越 <guoyue@wajiu.com>
2020-01-11 13:36:19 +0800

    'add_code'

--- code/LR.py
+++ code/LR.py
@@ -20,9 +20,9 @@ def create_data():
 
 class LogisticRegressionClassifier():
 
-    def __init__(self, max_itr=200,learning_rate=0.2):
+    def __init__(self, max_itr=200, alpha=0.2):
         self.max_itr = max_itr
-        self.learning_rate = learning_rate
+        self.alpha = alpha
 
     def sigmod(self,x):
         return 1 / (1 + exp(-x))
@@ -30,7 +30,7 @@ class LogisticRegressionClassifier():
     def fit(self,x, y):
         y = y.reshape([-1,1])
         bias = np.ones([x.shape[0]]).reshape([-1,1])
-        x = np.c_[x,bias]
+        x = np.c_[x,bias] # 加偏执项
         self.shape1 = x.shape[1]
         self.theta = np.random.normal(0,1,[self.shape1,1])
         # self.theta = np.array([2.,1.,-15.3]).reshape([self.shape1, 1])
@@ -44,13 +44,13 @@ class LogisticRegressionClassifier():
             # model = model * x
             model = np.mean(model,axis=0)
             model = model.reshape([-1,1])
-            self.theta -= self.learning_rate * model
+            self.theta -= self.alpha * model
             print(self.theta)
             # self.draw(x, y)
 
     def predict(self,x):
         bias = np.ones([x.shape[0]]).reshape([-1,1])
-        x = np.c_[x,bias]
+        x = np.c_[x,bias] # 加偏置项
         self.shape1 = x.shape[1]
         proba = self.sigmod(np.dot(x, self.theta))
         return proba > 0.5
@@ -70,8 +70,9 @@ if __name__ == '__main__':
     print(np.random.normal(0,1,[5,1]))
     np.random.seed(1)
 
-    files = np.load("homework.npz")
-    X = files['X']
+    x1 = np.random.normal(0,3,[2000]).reshape([-1,1])
+    x2 = np.random.normal(0,3,[2000]).reshape([-1,1])
+    X = np.c_[x1,x2]
     label_one_hot = []
     label = []
     for x1, x2 in X:
@@ -88,9 +89,11 @@ if __name__ == '__main__':
     label = np.array(label).reshape([-1,1])
     print(label_one_hot)
     print(label)
+    # x3 = X[:,0] * X[:,1]
+    # X = np.c_[X,x3]
 
     # x_train, x_test, y_train, y_test = train_test_split(X,label, random_state=1)
-    X, label = create_data()
+    # X, label = create_data()
     x_train, x_test, y_train, y_test = train_test_split(X,label, random_state=1)
     print(x_train.shape)
 
@@ -106,6 +109,9 @@ if __name__ == '__main__':
     x_points = np.arange(4,8,0.01)
     y_ = -(x_points * theta0 + theta2) / theta1
 
+    # x1 = np.random.normal(0,3,[2000])
+    # x2 = np.random.normal(0,3,[2000])
+    # plt.scatter(x1,x2)
     plt.scatter(x_test[:,0],x_test[:,1], c=result.reshape([-1]))
     # plt.scatter(x_train[:,0],x_train[:,1], c=y_train.reshape([-1]))
     plt.plot(x_points,y_, c='b')
---
+++ code/LinearRegression.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def model(x, a, b, c):
+    return a*x**2 + b*x + c
+
+def gradf(x, d, a, b, c):
+    y = model(x, a, b, c)
+    grada = 2 * (y - d) * x**2
+    gradb = 2 * (y - d) * x
+    gradc = 2 * (y - d)
+    return [grada, gradb, gradc]
+
+
+x = np.random.normal(1,1,[1000])
+d = x ** 2 + 2 * x + np.random.normal(0,0.4,[1000])
+
+a, b, c = 0, 0, 1    # 初始值
+batch_size = 10      # 每次训练样本的个数
+eta = 0.01            # 学习率
+
+for itr in range(1000):
+    idx = np.random.randint(0, 1000, batch_size)
+    xin = x[idx]
+    din = d[idx]
+    ga, gb, gc = gradf(xin,din,a,b,c)
+    ga = np.mean(ga)
+    gb = np.mean(gb)
+    gc = np.mean(gc)
+    a = a - ga * eta
+    b = b - gb * eta
+    c = c - gc * eta
+    print(a,b,c)
+
+
+xplt = np.linspace(-3, 5, 1000)
+ypred = model(xplt, a, b, c)
+
+plt.scatter(x, d)
+# plt.plot(xplt, ypred, lw=2)
+plt.show()
+
+# 预测过程
+import matplotlib
+matplotlib.rcParams['font.sans-serif'] = 'SimHei'
+# 输入x
+X = np.linspace(-2, 4, 100)
+# 输出y
+y = model(X, a, b, c)
+# 绘图
+# plt.scatter(x[:, 0], d[:, 0], s=20, alpha=0.4, label="数据散点")
+plt.scatter(x, d, s=20, alpha=0.4, label="数据散点")
+plt.plot(X, y, lw=5, color="#990000", alpha=0.5, label="预测关系")
+plt.legend()
+plt.show()
